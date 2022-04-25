6e3313c9e58a99e9920798273848ed14c92c9df7
zhongqi <869736098@qq.com>
2022-04-25 13:02:06 +0100

    start webhook

--- src/main/java/edu/tus/jpademo/controllers/JpaDemoController.java
+++ src/main/java/edu/tus/jpademo/controllers/JpaDemoController.java
@@ -18,7 +18,7 @@ public class JpaDemoController
 
     @GetMapping("/hello")
     public String sayHi() {
-        return "hello from jenkins test1";
+        return "hello from jenkins test1  --> start webhook";
     }
     
     @GetMapping("/users")
