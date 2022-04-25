6a6b49c991a779838b2be5ace4b90c4f83055b2b
zhongqi <869736098@qq.com>
2022-04-25 13:55:24 +0100

    commit teste

--- src/main/java/edu/tus/jpademo/controllers/JpaDemoController.java
+++ src/main/java/edu/tus/jpademo/controllers/JpaDemoController.java
@@ -18,7 +18,7 @@ public class JpaDemoController
 
     @GetMapping("/hello")
     public String sayHi() {
-        return "hello from jenkins test1  --> start webhook";
+        return "hello from jenkins-testweb";
     }
     
     @GetMapping("/users")
